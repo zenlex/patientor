eefb47c79dee7d5ce65097c9d5adb9ad76119070
zenlex <erichrkeil@gmail.com>
2022-04-18 13:18:24 -0500

    ex 9.19-9.20 complete

--- src/PatientInfoPage/index.tsx
+++ src/PatientInfoPage/index.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { apiBaseUrl } from "../constants";
 import { useStateValue, cachePatientDetails } from '../state';
-import { Patient } from '../types';
+import { Patient, Entry } from '../types';
 const PatientInfo = () => {
   const [{ patients }, dispatch] = useStateValue();
   const { id } = useParams<{ id: string }>();
@@ -36,8 +36,24 @@ const PatientInfo = () => {
         <li>DOB: {patient.dateOfBirth || 'unknown'}</li>
         <li>SSN: {patient.ssn || 'unknown'}</li>
         <li>Occupation: {patient.occupation}</li>
-      </ul>
-    </div>
+        <li>Entries: {patient.entries && patient.entries.map((entry: Entry) => (
+          <ul key={entry.id}>
+            <h4>{entry.date} - {entry.type}</h4>
+            <li>Description:{entry.description}</li>
+            <li>Specialist:{entry.specialist}</li>
+            {entry.diagnosisCodes
+              && <ul>
+                <h4>Diagnostic Codes:</h4>
+                {entry.diagnosisCodes.map((code: string) => (
+                  <li key={code}> {code}</li>
+                ))}
+              </ul>
+            }
+            {/*TODO: add conditional rendering of entry types fields*/}
+          </ul>
+        ))}</li>
+      </ul >
+    </div >
   );
 };
 
